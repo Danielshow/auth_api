3f972f6b3b8de17984393d1ea88048da65fe7eb1
danielshow <danielshotonwa53@gmail.com>
2019-07-31 15:33:34 +0100

    add trackable features for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :trackable, :rememberable, :validatable,
          omniauth_providers: %I[facebook google]
   include DeviseTokenAuth::Concerns::User
 end
