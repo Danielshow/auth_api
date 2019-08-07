8876f074f905265f7f1372d2c9ba195844f58525
danielshow <danielshotonwa53@gmail.com>
2019-08-07 12:56:56 +0100

    add profile during signup

--- app/controllers/overrides/sessions_controller.rb
+++ app/controllers/overrides/sessions_controller.rb
@@ -4,9 +4,14 @@ module Overrides
   # Override session controller
   class SessionsController < DeviseTokenAuth::SessionsController
     def render_create_success
+      profile = Profile.where(user_id: @resource.token_validation_response['id']).first
+      response = {
+        user: @resource.token_validation_response,
+        profile: profile.as_json
+      }
       render json: {
         message: 'success',
-        token: JsonWebToken.encode(@resource.token_validation_response)
+        token: JsonWebToken.encode(response)
       }
     end
   end
